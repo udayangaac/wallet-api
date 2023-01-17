e85918f988667adfdcf193bb964f5dc7349ffc43
Chamith Udayanga <udayangaac@gmail.com>
2023-01-17 19:25:58 +0530

    Update constants

--- models/constants.go
+++ models/constants.go
@@ -2,5 +2,10 @@
 // that have been modelled for use in wallet-api.
 package models
 
+import "time"
+
 // DateTimeFormat a domain specific date time format.
 const DateTimeFormat = "2006-01-02T15:04:05-07:00"
+
+// PeriodOfWalletHistory time difference between two wallet history records.
+const PeriodOfWalletHistory = 3600 * time.Second
